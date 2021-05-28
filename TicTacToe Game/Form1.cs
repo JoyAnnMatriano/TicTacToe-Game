fef20436108d7c164606f404511d0474ec859608
JoyAnnMatriano <ja.matriano24@gmail.com>
2021-05-28 13:34:09 +0800

    add ng mga if else para sa mga nanalo

--- TicTacToe Game/Form1.cs
+++ TicTacToe Game/Form1.cs
@@ -37,6 +37,9 @@ namespace TicTacToe_Game
                 btn.Text = "O";
             pasa = !pasa;
             btn.Enabled = false;
+            pasa_count++;
+
+            lookPanalo();
         }
 
         private void lookPanalo()
@@ -53,9 +56,53 @@ namespace TicTacToe_Game
                 may_nanalo_na = true;
 
 
+            //bx2
+            if ((bx2.Text == bx5.Text) && (bx5.Text == bx8.Text) && (!bx2.Enabled))
+                may_nanalo_na = true;
 
-            if(may_nanalo_na)
+
+            //bx3
+            if ((bx3.Text == bx6.Text) && (bx6.Text == bx9.Text) && (!bx3.Enabled))
+                may_nanalo_na = true;
+
+            if ((bx3.Text == bx5.Text) && (bx5.Text == bx7.Text) && (!bx3.Enabled))
+                may_nanalo_na = true;
+
+
+            //bx4
+            if ((bx4.Text == bx5.Text) && (bx5.Text == bx6.Text) && (!bx3.Enabled))
+                may_nanalo_na = true;
+
+            //bx 7
+            if ((bx7.Text == bx4.Text) && (bx4.Text == bx1.Text) && (!bx7.Enabled))
+                may_nanalo_na = true;
+            if ((bx7.Text == bx5.Text) && (bx5.Text == bx3.Text) && (!bx7.Enabled))
+                may_nanalo_na = true;
+            if ((bx7.Text == bx8.Text) && (bx8.Text == bx9.Text) && (!bx7.Enabled))
+                may_nanalo_na = true;
+
+
+            //bx 8
+            if ((bx8.Text == bx5.Text) && (bx5.Text == bx2.Text) && (!bx8.Enabled))
+                may_nanalo_na = true;
+
+
+            //bx9
+            if ((bx9.Text == bx8.Text) && (bx8.Text == bx7.Text) && (!bx9.Enabled))
+                may_nanalo_na = true;
+            if ((bx9.Text == bx5.Text) && (bx5.Text == bx1.Text) && (!bx9.Enabled))
+                may_nanalo_na = true;
+            if ((bx9.Text == bx6.Text) && (bx6.Text == bx3.Text) && (!bx9.Enabled))
+                may_nanalo_na = true;
+
+
+
+
+
+            if (may_nanalo_na)
             {
+                disablebuttons();
+
                 String nanalo = " ";
                 if (pasa)
                     nanalo = "O";
@@ -64,7 +111,33 @@ namespace TicTacToe_Game
 
 
                 MessageBox.Show(nanalo + "Wins!");
+            }//end ng if
+
+            else
+            {
+
+                if (pasa_count == 9)
+                    MessageBox.Show("Draw!");
+
+
             }
+
+
+
+        }//end ng look panalo
+
+        private void disablebuttons()
+
+        {
+
+            foreach (Component c in Controls)
+            {
+                Button btn = (Button)c;
+                btn.Enabled = false;
+
+            }//foreach end
+
+
         }
     }
 }
